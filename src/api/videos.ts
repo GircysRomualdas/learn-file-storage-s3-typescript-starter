fdb6dc3bae2cdfea63b7c78efb39bde550ce3e01
Romas <r2udis@gmail.com>
2026-02-06 00:48:40 +0200

    Add video upload

--- src/api/videos.ts
+++ src/api/videos.ts
@@ -1,8 +1,79 @@
 import { respondWithJSON } from "./json";
-
+import { getFileType } from "./assets";
 import { type ApiConfig } from "../config";
 import type { BunRequest } from "bun";
+import { unlink } from "fs/promises";
+import { BadRequestError, NotFoundError, UserForbiddenError } from "./errors";
+import { getBearerToken, validateJWT } from "../auth";
+import path from "path";
+import { randomBytes } from "crypto";
+import { getVideo, updateVideo } from "../db/videos";
 
 export async function handlerUploadVideo(cfg: ApiConfig, req: BunRequest) {
+  const { videoId } = req.params as { videoId?: string };
+  if (!videoId) {
+    throw new BadRequestError("Invalid video ID");
+  }
+
+  const token = getBearerToken(req.headers);
+  const userID = validateJWT(token, cfg.jwtSecret);
+
+  console.log("uploading video", videoId, "by user", userID);
+
+  const video = getVideo(cfg.db, videoId);
+  if (!video) {
+    throw new NotFoundError("Couldn't find video");
+  }
+
+  if (video.userID !== userID) {
+    throw new UserForbiddenError("User is not the owner of this video");
+  }
+
+  const formData = await req.formData();
+  const file = formData.get("video");
+  if (!(file instanceof File)) {
+    throw new BadRequestError("Video file missing");
+  }
+
+  const MAX_UPLOAD_SIZE = 1 << 30;
+  if (file.size > MAX_UPLOAD_SIZE) {
+    throw new BadRequestError("Video file is too large");
+  }
+
+  const mediaType = file.type;
+  if (!mediaType) {
+    throw new BadRequestError("Missing Content-Type for video");
+  }
+  if (mediaType !== "video/mp4") {
+    throw new BadRequestError("Invalid file type. Only MP4 allowed.");
+  }
+
+  const arrayBuffer = await file.arrayBuffer();
+  if (!arrayBuffer) {
+    throw new Error("Error reading file data");
+  }
+
+  const fileType = getFileType(mediaType);
+  if (!fileType) {
+    throw new Error("Unsupported or invalid file type");
+  }
+
+  const fileName = randomBytes(32).toString("base64url");
+  const key = `${fileName}.${fileType}`;
+  const tempFilePath = path.join(cfg.assetsRoot, `${fileName}.${fileType}`);
+  await Bun.write(tempFilePath, arrayBuffer);
+
+  try {
+    const localFile = Bun.file(tempFilePath);
+    const s3File = cfg.s3Client.file(key);
+    await s3File.write(localFile);
+  } finally {
+    await unlink(tempFilePath);
+  }
+
+  const videoURL = `https://${cfg.s3Bucket}.s3.${cfg.s3Region}.amazonaws.com/${fileName}.${fileType}`;
+  video.videoURL = videoURL;
+  updateVideo(cfg.db, video);
+
   return respondWithJSON(200, null);
 }
